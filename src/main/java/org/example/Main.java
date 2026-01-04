114d88a6e8ed7d15485cb440025078db2c45b1c8
Nandini390 <Shara.nandini123@gmail.com>
2026-01-05 02:19:09 +0530

    Added Retrofit support with GSON

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,26 +3,43 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
+    public static void main(String[] args) throws IOException {
+//        OkHttpClient client = new OkHttpClient();
+//        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//
+//        Request request= new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        try(Response response = client.newCall(request).execute()){
+//            if(!response.isSuccessful()){
+//                System.out.println("Something went wrong");
+//            }
+//            System.out.println(response.body().string());
+//        }catch(IOException ex){
+//            ex.printStackTrace();
+//        }
 
-        Request request= new Request.Builder()
-                .url(url)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        try(Response response = client.newCall(request).execute()){
-            if(!response.isSuccessful()){
-                System.out.println("Something went wrong");
-            }
-            System.out.println(response.body().string());
-        }catch(IOException ex){
-            ex.printStackTrace();
+        ToDoService service = retrofit.create(ToDoService.class);
+        retrofit2.Response<ToDo> response = service.getResponseById(3).execute();
+
+        if (response.isSuccessful() && response.body() != null) {
+            System.out.println(response.body());
+        } else {
+            System.out.println("Request failed");
         }
+
     }
 }
 
